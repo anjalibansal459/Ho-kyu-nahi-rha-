55c9df54d302e1b848551d69587b1a30d4480787
anjalibansal459 <76689035+anjalibansal459@users.noreply.github.com>
2021-07-09 14:37:49 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,64 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+   
+    
+    //write code to change the background color here
+    background("yellow")
+    //write code to show a heading for showing the result of Quiz
+    fill("black")
+text("Result Of The Quiz",350,50)
+    //call getContestantInfo( ) here
+    Contestant.getContestantInfo()
+    //write condition to check if contestantInfor is not undefined
+if(allContestants!==undefined){
+   //write code to add a note here
+  fill("blue")
+  textSize(20)
+  text("Player who answered correct is written with green colour ",100,500)
+}
+
+//write code to highlight contest who answered correctly
+for(var plr in allContestants){
+  var displayAns=300
+  var correctAns="2";
+  if(correctAns===allContestants[plr].answer)
+    fill("green")
+    else
+      fill("red")
+      displayAns+=50
+      text(allContestants[plr].name+allContestants[plr].answer,300,displayAns)
+  
+  }
+ 
+  }
+
+}
